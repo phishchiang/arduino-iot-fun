3ae444995a7c6b7ba167d26d3dd28db483e6d458
front_end_dev_002 <5311508+front_end_dev_002@user.noreply.gitee.com>
2019-10-17 18:50:06 +0800

    Basic commit for the websocketIO

---
+++ app.js
@@ -0,0 +1,31 @@
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+const cors = require('cors');
+
+app.use(cors());
+
+users = [];
+connections = [];
+
+app.get('/client', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+});
+
+io.on('connection', function(socket) {
+  connections.push(socket);
+  console.log(`${socket.id} connected`);
+  socket.on('chat message', function(data) {
+    console.log('message: ' + data);
+    io.emit('All', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`${socket.id} is gone`);
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
